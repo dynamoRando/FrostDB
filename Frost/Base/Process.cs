0afac3eb057b439d542231f16ace59b498ec64f2
Randy Le <randohernandez0@gmail.com>
2019-09-24 21:38:14 -0700

    project setup

---
+++ Frost/Base/Process.cs
@@ -0,0 +1,29 @@
+﻿using Frost.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Frost.Base
+{
+    public class Process : IProcess
+    {
+        #region Private Fields
+        #endregion
+
+        #region Public Properties
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Public Methods
+        #endregion
+
+        #region Private Methods
+        #endregion
+
+    }
+}
---
+++ Frost/Instance/Instance.cs
@@ -0,0 +1,11 @@
+﻿using Frost.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Frost.Instance
+{
+    public class Instance : Process
+    {
+    }
+}
