00be6113635c62463d99a6be6a6b39aa439d7afd
Randy Le <randohernandez0@gmail.com>
2019-09-25 01:19:44 -0700

    asdf

---
+++ Frost/Base/Row.cs
@@ -0,0 +1,14 @@
+﻿using FrostDB.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrostDB.Base
+{
+    public class Row : IRow
+    {
+        public Guid Id => throw new NotImplementedException();
+        public List<IColumn> Columns => throw new NotImplementedException();
+        public List<IRowValue> Values => throw new NotImplementedException();
+    }
+}
