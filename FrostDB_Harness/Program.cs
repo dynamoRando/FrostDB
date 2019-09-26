0c3ce104f7102e08dbd73aa3bff76c0e38fce94c
Randy Le <randohernandez0@gmail.com>
2019-09-25 20:24:07 -0700

    d

---
+++ FrostDB_Harness/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FrostDB_Harness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
