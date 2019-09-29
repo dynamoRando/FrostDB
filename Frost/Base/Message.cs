143b03ad24fa4157656a39c3a308dccd4fef9948
Randy Le <randohernandez0@gmail.com>
2019-09-28 22:33:23 -0700

    asdf

--- Frost/Base/Message.cs
+++ Frost/Base/Message.cs
@@ -7,8 +7,27 @@ namespace FrostDB.Base
 {
     public class Message : IMessage
     {
+        #region Private Fields
+        #endregion
+
+        #region Public Properties
         public Guid Id { get; set; }
-        public Location Destination { get; set; }
-        public Location Origin { get; set; }
+        public ILocation Destination { get; set; }
+        public ILocation Origin { get; set; }
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Public Methods
+        #endregion
+
+        #region Private Methods
+        #endregion
+
+
     }
 }
