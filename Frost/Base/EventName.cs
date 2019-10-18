87e23f2e62010f763c6efe5f75618a1ff7632f3c
Randy Le <randohernandez0@gmail.com>
2019-10-18 10:05:39 -0700

    added events

--- Frost/Base/EventName.cs
+++ Frost/Base/EventName.cs
@@ -9,12 +9,20 @@ namespace FrostDB.Base
         public class Database
         {
             public const string Created = "Database_Created";
+            public const string Deleted = "Database_Deleted";
         }
 
         public class Table
         {
             public const string Created = "Table_Created";
             public const string Dropped = "Table_Dropped";
+        }
+        
+        public class Row
+        {
+            public const string Added = "Row_Added";
+            public const string Modified = "Row_Modified";
+            public const string Deleted = "Row_Deleted";
         }
     }
     
