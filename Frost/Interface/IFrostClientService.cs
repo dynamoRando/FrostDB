5cc4475ae2e22d749edeae204590a2ee60e2b199
Randy Le <randyL2004@gmail.com>
2019-11-17 14:36:19 -0800

    scaffolding

---
+++ Frost/Interface/IFrostClientService.cs
@@ -0,0 +1,14 @@
+﻿using FrostDB.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FrostDB.Interface.IResults;
+
+namespace FrostDB.Interface
+{
+    public interface IFrostClientService
+    {
+        IRegisterDatabaseResult RegisterDatabase(Contract contract);
+        IAddRowToDatabaseResult AddRowToDatabase(Participant participant, Guid? DatabaseId, Row row);
+    }
+}
---
+++ Frost/Interface/IFrostClientServiceResult/IResults.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrostDB.Interface.IResults
+{
+    public interface IRegisterDatabaseResult
+    {
+    }
+
+    public interface IAddRowToDatabaseResult
+    { }
+}
