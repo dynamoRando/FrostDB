12a8ae0779a9d3bd25ca9a7a5903db568183c720
Randy Le <randohernandez0@gmail.com>
2019-11-29 11:50:44 -0800

    asdf

--- Frost/Classes/Client.cs
+++ Frost/Classes/Client.cs
@@ -16,9 +16,8 @@ namespace FrostDB
         new ManualResetEvent(false);
         private static ManualResetEvent sendDone =
             new ManualResetEvent(false);
-        private static ManualResetEvent receiveDone =
-            new ManualResetEvent(false);
-        private static String response = String.Empty;
+        //private static ManualResetEvent receiveDone = new ManualResetEvent(false);
+        //private static String response = String.Empty;
         #endregion
 
         #region Public Properties
@@ -63,6 +62,8 @@ namespace FrostDB
                 client.Shutdown(SocketShutdown.Both);
                 client.Close();
                 client.Dispose();
+                connectDone.Dispose();
+                sendDone.Dispose();
 
                 EventManager.TriggerEvent(EventName.Message.Message_Sent, CreateMessageSentEventArgs(message));
             }
@@ -132,61 +133,61 @@ namespace FrostDB
             }
         }
 
-        private static void Receive(Socket client)
-        {
-            try
-            {
-                // Create the state object.  
-                StateObject state = new StateObject();
-                state.workSocket = client;
+        //private static void Receive(Socket client)
+        //{
+        //    try
+        //    {
+        //        // Create the state object.  
+        //        StateObject state = new StateObject();
+        //        state.workSocket = client;
 
-                // Begin receiving the data from the remote device.  
-                client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
-                    new AsyncCallback(ReceiveCallback), state);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-        }
+        //        // Begin receiving the data from the remote device.  
+        //        client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+        //            new AsyncCallback(ReceiveCallback), state);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine(e.ToString());
+        //    }
+        //}
 
-        private static void ReceiveCallback(IAsyncResult ar)
-        {
-            try
-            {
-                // Retrieve the state object and the client socket   
-                // from the asynchronous state object.  
-                StateObject state = (StateObject)ar.AsyncState;
-                Socket client = state.workSocket;
+        //private static void ReceiveCallback(IAsyncResult ar)
+        //{
+        //    try
+        //    {
+        //        // Retrieve the state object and the client socket   
+        //        // from the asynchronous state object.  
+        //        StateObject state = (StateObject)ar.AsyncState;
+        //        Socket client = state.workSocket;
 
-                // Read data from the remote device.  
-                int bytesRead = client.EndReceive(ar);
+        //        // Read data from the remote device.  
+        //        int bytesRead = client.EndReceive(ar);
 
-                if (bytesRead > 0)
-                {
-                    // There might be more data, so store the data received so far.  
-                    state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+        //        if (bytesRead > 0)
+        //        {
+        //            // There might be more data, so store the data received so far.  
+        //            state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
 
-                    // Get the rest of the data.  
-                    client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
-                        new AsyncCallback(ReceiveCallback), state);
-                }
-                else
-                {
-                    // All the data has arrived; put it in response.  
-                    if (state.sb.Length > 1)
-                    {
-                        response = state.sb.ToString();
-                    }
-                    // Signal that all bytes have been received.  
-                    receiveDone.Set();
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-        }
+        //            // Get the rest of the data.  
+        //            client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+        //                new AsyncCallback(ReceiveCallback), state);
+        //        }
+        //        else
+        //        {
+        //            // All the data has arrived; put it in response.  
+        //            if (state.sb.Length > 1)
+        //            {
+        //                response = state.sb.ToString();
+        //            }
+        //            // Signal that all bytes have been received.  
+        //            receiveDone.Set();
+        //        }
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine(e.ToString());
+        //    }
+        //}
 
         private static MessageSentEventArgs CreateMessageSentEventArgs(Message message)
         {
