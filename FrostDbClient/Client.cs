c4aeda57615ecfb2bca390d3729983bc48fb008f
Randy Le <randohernandez0@gmail.com>
2019-11-29 10:56:02 -0800

    asdf

--- FrostDbClient/Client.cs
+++ FrostDbClient/Client.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Net;
 
 namespace FrostDbClient
 {
     public class Client
     {
+        public void Connect(IPAddress address, int portNumber)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
