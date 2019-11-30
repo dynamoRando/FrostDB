15a4b5f4514aad52a20974e177b99985903ef2e7
Randy Le <randohernandez0@gmail.com>
2019-11-29 16:05:48 -0800

    put together outline of console

--- FrostConsole/App.cs
+++ FrostConsole/App.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using System.Text;
 using FrostDB;
+using System.Threading.Tasks;
 
 namespace FrostConsole
 {
     public static class App
     {
         #region Private Fields
+        static bool keepRunning = true;
         #endregion
 
         #region Public Properties
         public static Process Process { get; set; }
+        public static bool IsRunning => keepRunning;
         #endregion
 
         #region Protected Methods
@@ -26,20 +29,28 @@ namespace FrostConsole
         #region Public Methods
         public static void Configure()
         {
-
+            throw new NotImplementedException();
         }
-        public static void Run()
+        public static void Startup()
         {
             if (Process is null)
             {
                 Process = new Process();
                 Process.LoadDatabases();
                 Process.StartRemoteServer();
+                Process.StartConsoleServer();
+                keepRunning = true;
+                OutputProcessInfo();
             }
         }
         public static void Shutdown()
         {
-
+            if (Process != null)
+            {
+                Process.StopConsoleServer();
+                Process.StopRemoteServer();
+                keepRunning = false;
+            }
         }
         public static string Prompt(string message)
         {
@@ -50,6 +61,18 @@ namespace FrostConsole
         #endregion
 
         #region Private Methods
+        private static void OutputProcessInfo()
+        {
+            Console.WriteLine("==========================");
+            Console.WriteLine("FrostDb Process Information");
+            Console.WriteLine($"Id: {Process.Id }");
+            Console.WriteLine($"Ip Address: {Process.Configuration.Address.ToString() }");
+            Console.WriteLine($"Data Port Number: {Process.Configuration.DataServerPort.ToString() }");
+            Console.WriteLine($"Console Port Number: {Process.Configuration.ConsoleServerPort.ToString() }");
+            Console.WriteLine($"Database Folder: {Process.Configuration.DatabaseFolder }");
+            Console.WriteLine($"Contract Folder: {Process.Configuration.ContractFolder}");
+            Console.WriteLine("==========================");
+        }
         #endregion
 
 
--- FrostConsole/Program.cs
+++ FrostConsole/Program.cs
@@ -6,23 +6,37 @@ namespace FrostConsole
     {
         static void Main(string[] args)
         {
-            var result = App.Prompt("Would you like to (s) - start FrostDb or (c) - configure your instance? (e) to exit.");
+            string result;
 
             do
             {
+                result = App.Prompt("Would you like to (s) - start FrostDb or (c) - configure your instance? (e) to exit.");
                 switch (result)
                 {
                     case "s":
-                        App.Run();
+                        App.Startup();
+
+                        while (App.IsRunning)
+                        {
+                            result = App.Prompt("FrostDb is running, press (e) key to exit");
+
+                            if (result == "e")
+                            {
+                                App.Shutdown();
+                                break;
+                            }
+                        }
+
                         break;
                     case "c":
                         App.Configure();
                         break;
                     case "e":
-                        App.Shutdown();
                         break;
                 }
             } while (result != "e");
+
+            Console.WriteLine("Console finished running.");
         }
     }
 }
