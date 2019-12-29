dd2bc277d6bdb891ad90e264d66fedc3999f985d
Randy Le <randohernandez0@gmail.com>
2019-12-28 21:04:18 -0700

    add more forms

---
+++ FrostForm/formManageContract.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace FrostForm
+{
+    partial class formManageContract
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelDatabase = new System.Windows.Forms.Label();
+            this.labelDatabaseName = new System.Windows.Forms.Label();
+            this.labelDescription = new System.Windows.Forms.Label();
+            this.textboxDescription = new System.Windows.Forms.TextBox();
+            this.labelSchema = new System.Windows.Forms.Label();
+            this.textboxSchema = new System.Windows.Forms.TextBox();
+            this.labelParticipantDataOverview = new System.Windows.Forms.Label();
+            this.textboxParticipantDataOverview = new System.Windows.Forms.TextBox();
+            this.labelAuthorDataOverview = new System.Windows.Forms.Label();
+            this.textboxAuthorDataOverview = new System.Windows.Forms.TextBox();
+            this.buttonSaveContract = new System.Windows.Forms.Button();
+            this.textboxParticipantDataOverviewRemarks = new System.Windows.Forms.TextBox();
+            this.textboxAuthorDataOverviewRemarks = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelDatabase
+            // 
+            this.labelDatabase.AutoSize = true;
+            this.labelDatabase.Location = new System.Drawing.Point(22, 27);
+            this.labelDatabase.Name = "labelDatabase";
+            this.labelDatabase.Size = new System.Drawing.Size(58, 15);
+            this.labelDatabase.TabIndex = 0;
+            this.labelDatabase.Text = "Database:";
+            // 
+            // labelDatabaseName
+            // 
+            this.labelDatabaseName.AutoSize = true;
+            this.labelDatabaseName.Location = new System.Drawing.Point(86, 27);
+            this.labelDatabaseName.Name = "labelDatabaseName";
+            this.labelDatabaseName.Size = new System.Drawing.Size(87, 15);
+            this.labelDatabaseName.TabIndex = 1;
+            this.labelDatabaseName.Text = "DatabaseName";
+            // 
+            // labelDescription
+            // 
+            this.labelDescription.AutoSize = true;
+            this.labelDescription.Location = new System.Drawing.Point(22, 62);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(70, 15);
+            this.labelDescription.TabIndex = 2;
+            this.labelDescription.Text = "Description:";
+            // 
+            // textboxDescription
+            // 
+            this.textboxDescription.Location = new System.Drawing.Point(22, 80);
+            this.textboxDescription.Multiline = true;
+            this.textboxDescription.Name = "textboxDescription";
+            this.textboxDescription.Size = new System.Drawing.Size(766, 119);
+            this.textboxDescription.TabIndex = 3;
+            // 
+            // labelSchema
+            // 
+            this.labelSchema.AutoSize = true;
+            this.labelSchema.Location = new System.Drawing.Point(22, 218);
+            this.labelSchema.Name = "labelSchema";
+            this.labelSchema.Size = new System.Drawing.Size(52, 15);
+            this.labelSchema.TabIndex = 4;
+            this.labelSchema.Text = "Schema:";
+            // 
+            // textboxSchema
+            // 
+            this.textboxSchema.Location = new System.Drawing.Point(22, 236);
+            this.textboxSchema.Multiline = true;
+            this.textboxSchema.Name = "textboxSchema";
+            this.textboxSchema.Size = new System.Drawing.Size(766, 139);
+            this.textboxSchema.TabIndex = 5;
+            // 
+            // labelParticipantDataOverview
+            // 
+            this.labelParticipantDataOverview.AutoSize = true;
+            this.labelParticipantDataOverview.Location = new System.Drawing.Point(22, 395);
+            this.labelParticipantDataOverview.Name = "labelParticipantDataOverview";
+            this.labelParticipantDataOverview.Size = new System.Drawing.Size(146, 15);
+            this.labelParticipantDataOverview.TabIndex = 6;
+            this.labelParticipantDataOverview.Text = "Participant Data Overview:";
+            // 
+            // textboxParticipantDataOverview
+            // 
+            this.textboxParticipantDataOverview.Location = new System.Drawing.Point(22, 413);
+            this.textboxParticipantDataOverview.Multiline = true;
+            this.textboxParticipantDataOverview.Name = "textboxParticipantDataOverview";
+            this.textboxParticipantDataOverview.Size = new System.Drawing.Size(766, 102);
+            this.textboxParticipantDataOverview.TabIndex = 7;
+            // 
+            // labelAuthorDataOverview
+            // 
+            this.labelAuthorDataOverview.AutoSize = true;
+            this.labelAuthorDataOverview.Location = new System.Drawing.Point(22, 716);
+            this.labelAuthorDataOverview.Name = "labelAuthorDataOverview";
+            this.labelAuthorDataOverview.Size = new System.Drawing.Size(126, 15);
+            this.labelAuthorDataOverview.TabIndex = 8;
+            this.labelAuthorDataOverview.Text = "Author Data Overview:";
+            // 
+            // textboxAuthorDataOverview
+            // 
+            this.textboxAuthorDataOverview.Location = new System.Drawing.Point(22, 734);
+            this.textboxAuthorDataOverview.Multiline = true;
+            this.textboxAuthorDataOverview.Name = "textboxAuthorDataOverview";
+            this.textboxAuthorDataOverview.Size = new System.Drawing.Size(766, 132);
+            this.textboxAuthorDataOverview.TabIndex = 9;
+            // 
+            // buttonSaveContract
+            // 
+            this.buttonSaveContract.Location = new System.Drawing.Point(673, 1067);
+            this.buttonSaveContract.Name = "buttonSaveContract";
+            this.buttonSaveContract.Size = new System.Drawing.Size(115, 23);
+            this.buttonSaveContract.TabIndex = 10;
+            this.buttonSaveContract.Text = "Save Contract";
+            this.buttonSaveContract.UseVisualStyleBackColor = true;
+            // 
+            // textboxParticipantDataOverviewRemarks
+            // 
+            this.textboxParticipantDataOverviewRemarks.Location = new System.Drawing.Point(22, 521);
+            this.textboxParticipantDataOverviewRemarks.Multiline = true;
+            this.textboxParticipantDataOverviewRemarks.Name = "textboxParticipantDataOverviewRemarks";
+            this.textboxParticipantDataOverviewRemarks.Size = new System.Drawing.Size(766, 192);
+            this.textboxParticipantDataOverviewRemarks.TabIndex = 11;
+            // 
+            // textboxAuthorDataOverviewRemarks
+            // 
+            this.textboxAuthorDataOverviewRemarks.Location = new System.Drawing.Point(22, 873);
+            this.textboxAuthorDataOverviewRemarks.Multiline = true;
+            this.textboxAuthorDataOverviewRemarks.Name = "textboxAuthorDataOverviewRemarks";
+            this.textboxAuthorDataOverviewRemarks.Size = new System.Drawing.Size(766, 188);
+            this.textboxAuthorDataOverviewRemarks.TabIndex = 12;
+            // 
+            // formManageContract
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 1100);
+            this.Controls.Add(this.textboxAuthorDataOverviewRemarks);
+            this.Controls.Add(this.textboxParticipantDataOverviewRemarks);
+            this.Controls.Add(this.buttonSaveContract);
+            this.Controls.Add(this.labelSchema);
+            this.Controls.Add(this.textboxAuthorDataOverview);
+            this.Controls.Add(this.labelAuthorDataOverview);
+            this.Controls.Add(this.textboxParticipantDataOverview);
+            this.Controls.Add(this.labelParticipantDataOverview);
+            this.Controls.Add(this.textboxSchema);
+            this.Controls.Add(this.labelDescription);
+            this.Controls.Add(this.textboxDescription);
+            this.Controls.Add(this.labelDatabaseName);
+            this.Controls.Add(this.labelDatabase);
+            this.Name = "formManageContract";
+            this.Text = "formManageContract";
+            this.Load += new System.EventHandler(this.formManageContract_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelDatabase;
+        private System.Windows.Forms.Label labelDatabaseName;
+        private System.Windows.Forms.Label labelDescription;
+        private System.Windows.Forms.TextBox textboxDescription;
+        private System.Windows.Forms.Label labelSchema;
+        private System.Windows.Forms.TextBox textboxSchema;
+        private System.Windows.Forms.Label labelParticipantDataOverview;
+        private System.Windows.Forms.TextBox textboxParticipantDataOverview;
+        private System.Windows.Forms.Label labelAuthorDataOverview;
+        private System.Windows.Forms.TextBox textboxAuthorDataOverview;
+        private System.Windows.Forms.Button buttonSaveContract;
+        private System.Windows.Forms.TextBox textboxParticipantDataOverviewRemarks;
+        private System.Windows.Forms.TextBox textboxAuthorDataOverviewRemarks;
+    }
+}
---
+++ FrostForm/formManageContract.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FrostForm
+{
+    public partial class formManageContract : Form
+    {
+        App _app;
+        string _databaseName;
+
+        public formManageContract(App app, string databaseName)
+        {
+            InitializeComponent();
+            _app = app;
+            _databaseName = databaseName;
+        }
+
+        private void formManageContract_Load(object sender, EventArgs e)
+        {
+            labelDatabaseName.Text = _databaseName;
+        }
+    }
+}
