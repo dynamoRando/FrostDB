8737b0b03b5cf8ee3dd1f8499ace292775dd3d84
Randy Le <randohernandez0@gmail.com>
2020-01-01 09:57:14 -0700

    stff

---
+++ FrostCommon/ConsoleMessages/ContractInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrostCommon.ConsoleMessages
+{
+    public class ContractInfo
+    {
+        public string DatabaseName { get; set; }
+        public Guid? ContractId { get; set; }
+        public Guid? ContractVersion { get; set; }
+        public List<string> TableNames { get; set; }
+        
+    }
+}
