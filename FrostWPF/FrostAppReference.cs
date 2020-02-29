eaf9a55cef538592b29f4331c4fde7fc9fd53e6b
Randy Le <randohernandez0@gmail.com>
2020-02-29 10:52:41 -0700

    add new WPF version of Frost Desktop app since WinForms keeps bombing out in Visual Studio Preview

---
+++ FrostWPF/FrostAppReference.cs
@@ -0,0 +1,12 @@
+﻿using FrostDbClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrostWPF
+{
+    public static class FrostAppReference
+    {
+        public static FrostClient Client { get; set; }
+    }
+}
