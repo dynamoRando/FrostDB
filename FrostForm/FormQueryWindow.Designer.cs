0cf7a6976a2345dd61646a4993e3c6b87bd86b12
Randy Le <randohernandez0@gmail.com>
2020-03-03 09:46:32 -0700

    modify query form

--- FrostForm/FormQueryWindow.Designer.cs
+++ FrostForm/FormQueryWindow.Designer.cs
@@ -28,12 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboDatabase = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboTable = new System.Windows.Forms.ComboBox();
+            this.buttonExecute = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.textQuery = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textResults = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textResults);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textQuery);
+            this.groupBox1.Controls.Add(this.buttonClear);
+            this.groupBox1.Controls.Add(this.buttonExecute);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.comboTable);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboDatabase);
+            this.groupBox1.Location = new System.Drawing.Point(27, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(399, 426);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Read Data";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Database";
+            // 
+            // comboDatabase
+            // 
+            this.comboDatabase.FormattingEnabled = true;
+            this.comboDatabase.Location = new System.Drawing.Point(67, 26);
+            this.comboDatabase.Name = "comboDatabase";
+            this.comboDatabase.Size = new System.Drawing.Size(313, 23);
+            this.comboDatabase.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Table";
+            // 
+            // comboTable
+            // 
+            this.comboTable.FormattingEnabled = true;
+            this.comboTable.Location = new System.Drawing.Point(67, 59);
+            this.comboTable.Name = "comboTable";
+            this.comboTable.Size = new System.Drawing.Size(313, 23);
+            this.comboTable.TabIndex = 3;
+            // 
+            // buttonExecute
+            // 
+            this.buttonExecute.Location = new System.Drawing.Point(305, 88);
+            this.buttonExecute.Name = "buttonExecute";
+            this.buttonExecute.Size = new System.Drawing.Size(75, 23);
+            this.buttonExecute.TabIndex = 4;
+            this.buttonExecute.Text = "Execute";
+            this.buttonExecute.UseVisualStyleBackColor = true;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(224, 88);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 5;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            // 
+            // textQuery
+            // 
+            this.textQuery.Location = new System.Drawing.Point(6, 137);
+            this.textQuery.Multiline = true;
+            this.textQuery.Name = "textQuery";
+            this.textQuery.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textQuery.Size = new System.Drawing.Size(374, 120);
+            this.textQuery.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-40, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Query";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 264);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Results";
+            // 
+            // textResults
+            // 
+            this.textResults.Location = new System.Drawing.Point(7, 282);
+            this.textResults.Multiline = true;
+            this.textResults.Name = "textResults";
+            this.textResults.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textResults.Size = new System.Drawing.Size(373, 138);
+            this.textResults.TabIndex = 10;
+            // 
+            // FormQueryWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FormQueryWindow";
             this.Text = "FormQueryWindow";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textQuery;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonExecute;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboTable;
+        private System.Windows.Forms.ComboBox comboDatabase;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textResults;
     }
 }
