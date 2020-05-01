104c12be5087f694b1b92a481376cc410db29edc
Randy Le <randyL2004@outlook.com>
2020-04-30 21:46:55 -0700

    add example queries to query window

---
+++ FrostForm/SelectQueryExample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrostForm
+{
+    class SelectQueryExample : IQueryExample
+    {
+        public string Name => "Select";
+
+        public string GetExample()
+        {
+            return @"select { * } from { testtable };";
+        }
+    }
+}
---
+++ FrostForm/UpdateQueryExample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrostForm
+{
+    class UpdateQueryExample : IQueryExample
+    {
+        public string Name => "Update";
+
+        public string GetExample()
+        {
+            return @"UPDATE { tableName } SET { col 1 = value1, col 2 = value 2 };";
+        }
+    }
+}
