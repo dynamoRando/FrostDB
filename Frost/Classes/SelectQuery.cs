e834f2ee1e3e8482f8a8943a96ca627c824d01d8
Randy Le <randohernandez0@gmail.com>
2020-05-12 22:18:11 -0700

    example comment

--- Frost/Classes/SelectQuery.cs
+++ Frost/Classes/SelectQuery.cs
@@ -115,6 +115,45 @@ namespace FrostDB
         #endregion
 
         #region Private Methods
+
+        /*
+         *  private const string WHERE = "WHERE";
+
+        public void ParseWhereClause(string statement)
+        {
+            // find the {} after the WHERE keyword
+            int whereClauseIndex = statement.IndexOf(WHERE);
+
+            // find the start position
+            int startPosition = whereClauseIndex + WHERE.Length;
+
+            // get the string from the start position to the end of the string 
+            string clause = statement.Substring(startPosition, statement.Length - startPosition);
+
+            // grab the text between the { } 
+            var item = clause.Split('{', '}');
+
+            // grab the ( ) groupings
+            var clauses = item.ToList();
+
+            foreach(var c in clauses)
+            {
+                string value = c.Trim();
+                if (value.Length > 0)
+                {
+                    var items = value.Split('(', ')');
+
+                    foreach(var i in items)
+                    {
+                        var x = i.Trim();
+                        Debug.WriteLine(x);
+                        Console.WriteLine(x);
+                    }
+                }
+            }
+        }
+         */
+
         private void ParseWhereClause(string statement)
         {
             // find the {} after the WHERE keyword
