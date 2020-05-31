1cb6489ae21a372b55eccc213ed0dc111c766137
Randy Le <randyL2004@gmail.com>
2020-05-31 16:30:09 -0700

    menu setup

--- FrostConsoleHarness/Program.cs
+++ FrostConsoleHarness/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using FrostDb;
+using FrostDB;
 
 namespace FrostConsoleHarness
 {
@@ -25,16 +25,50 @@ namespace FrostConsoleHarness
         {
             Console.WriteLine("Frost Console Harness");
             Console.WriteLine("Please choose an option:");
-            Console.WriteLine("(s) - start a new instance");
+            Console.WriteLine("(s) - start a new instance with default settings");
             Console.WriteLine("(c) - configure a new instance");
+            Console.WriteLine("(k) - kill a existing instance");
             Console.WriteLine("(e) - exit application");
 
             return Console.ReadLine();
         }
 
-        static string EvaluateInput(string input)
+        static void EvaluateInput(string input)
         {
-            
+            switch (input)
+            {
+                case "s":
+                    StartDefaultProcess();
+                    break;
+                case "c":
+                    ConfigureNewProcess();
+                    break;
+                case "k":
+                    KillExistingProcess();
+                    break;
+                default:
+                    // do nothing
+                    break;
+            }
         }
+
+        static void StartDefaultProcess()
+        {
+            Console.WriteLine("Start A New Default Process");
+            throw new NotImplementedException();
+        }
+
+        static void ConfigureNewProcess()
+        {
+            Console.WriteLine("Configure A New Process");
+            throw new NotImplementedException();
+        }
+
+        static void KillExistingProcess()
+        {
+            Console.WriteLine("Kill an existing process");
+            throw new NotImplementedException();
+        }
+
     }
 }
