939729ea802a5a5bfbc6e412225764b0282fac49
Randy Le <randohernandez0@gmail.com>
2020-06-28 15:20:19 -0700

    setup for object oriented querying

---
+++ FrostCommon/IInstruction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrostCommon
+{
+    public interface IInstruction
+    {
+        string DatabaseName { get; set; }
+        string TableName { get; set; }
+    }
+}
---
+++ FrostCommon/Instruction/Condition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrostCommon.Instruction
+{
+    [Serializable]
+    public class Condition
+    {
+        public string TableName { get; set; }
+        public int ConditionOrder { get; set; }
+        public string Statement { get; set; }
+        public string Boolean { get; set; }
+    }
+}
