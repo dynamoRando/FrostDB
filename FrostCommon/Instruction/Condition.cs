ebccec6e5bc625ac4ffc7103a27c23bd34f1e2aa
Randy Le <randohernandez0@gmail.com>
2020-06-30 21:24:34 -0700

    stuff

--- FrostCommon/Instruction/Condition.cs
+++ FrostCommon/Instruction/Condition.cs
@@ -11,5 +11,15 @@ namespace FrostCommon.Instruction
         public int ConditionOrder { get; set; }
         public string Statement { get; set; }
         public string Boolean { get; set; }
+        public Guid? Id { get; set; }
+
+        public Condition()
+        {
+            Id = Guid.NewGuid();
+        }
     }
+
+    /*
+     * Boolean is AND/OR. Two conditions must be at the same level for this to work
+     * */
 }
