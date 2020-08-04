b57fe5f12630a35f27c13bfa639a1921096a5cda
Randy Le <randohernandez0@gmail.com>
2020-08-03 21:18:37 -0700

    a

--- Frost/Query/DeleteStep.cs
+++ Frost/Query/DeleteStep.cs
@@ -4,7 +4,59 @@ using System.Text;
 
 namespace FrostDB
 {
-    public class DeleteStep
+    public class DeleteStep : IPlanStep
     {
+        #region Private Fields
+        #endregion
+
+        #region Public Properties
+        public Guid Id { get; set; }
+        public int Level { get; set; }
+        public string TableName { get; set; }
+        public string DatabaseName { get; set; }
+        public IPlanStep InputStep { get; set; }
+        public bool HasInputStep => CheckHasInputStep();
+        #endregion
+
+        #region Protected Methods
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Constructors
+        public DeleteStep()
+        {
+            Id = Guid.NewGuid();
+        }
+        #endregion
+
+        #region Public Methods
+        public StepResult GetResult(Process process, string databaseName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetResultText()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Private Methods
+        private bool CheckHasInputStep()
+        {
+            if (InputStep != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        #endregion
+
+
     }
 }
