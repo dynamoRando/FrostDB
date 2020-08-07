864b5677dd7d0d9a180aa6013f41c911158221e8
Randy Le <randohernandez0@gmail.com>
2020-08-07 08:41:43 -0700

    a

--- Frost/Storage/DbFill.cs
+++ Frost/Storage/DbFill.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 namespace FrostDB
 {
+    /// <summary>
+    /// Used to load a database from disk
+    /// </summary>
     public class DbFill : IDbFill
     {
         public DbSchema Schema { get; set; }
