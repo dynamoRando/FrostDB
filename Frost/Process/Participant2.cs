45e49424eb91ebb93f1fa649e7c35bbc9935f874
Randy Le <randyL2004@outlook.com>
2020-08-23 19:02:21 -0600

    a

---
+++ Frost/Process/Participant2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FrostCommon;
+
+namespace FrostDB
+{
+    public class Participant2
+    {
+
+        #region Private Fields
+        #endregion
+
+        #region Public Properties
+        public Guid? Id { get; set; }
+        public Location2 Location { get; set; }
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Public Methods
+        #endregion
+
+        #region Private Methods
+        #endregion
+
+    }
+}
---
+++ FrostCommon/Location2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrostCommon
+{
+    public class Location2
+    {
+
+        #region Private Fields
+        #endregion
+
+        #region Public Properties
+        public string IpAddress { get; set; }
+        public int PortNumber { get; set; }
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Public Methods
+        #endregion
+
+        #region Private Methods
+        #endregion
+
+    }
+}
